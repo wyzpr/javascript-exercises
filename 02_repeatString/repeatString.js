b8ab0dc0b32e620637acde8ac577845df7ba7bc5
wyzpr <jishnu.singha@proton.me>
2025-02-09 04:15:43 +0000

    Solved parts 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, number) {
+    let s = "";
+    if (number < 0) {
+        return "ERROR";
+    }
+    for (i = 0; i < number; i++) {
+        s += str;
+    }
+    return s;
 };
 
 // Do not edit below this line
